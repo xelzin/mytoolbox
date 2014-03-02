5d1eaea3bd69c64fc29fa2604815a0e5d02f8abc
Bkram <mrdebruijn@gmail.com>
2014-03-02 19:30:34 +0100

    Put all configuration items in a seperate configuration file.

--- mailtools/smtpd-dual-delivery.py
+++ mailtools/smtpd-dual-delivery.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
-# SMTP dual delivery relay (proxy) 
-# Warning no queueing implemented mail might get lost if one of the proxyto servers is not handling the mail correctly.
-# 2014 bkram
-
-# Set variables below
-
-# Listen on this ip and port
-bindip = '127.0.0.1'
-bindport = 25
-
-# Proxy to these SMTP servers
-proxytoip_a ='smtp.mydomain.tld'
-proxytoport_a='25'
-
-proxytoip_b ='smtp.otherdomain.tld'
-proxytoport_b='25'
+# SMTP dual delivery relay proxy daemon
+# Warning no queueing implemented mail might get lost if one of the receiving servers is not handling the mail correctly.
+# No Warranty whatsoever
+# (c) 2014 bkram
 
 import asyncore
 import smtpd
 import smtplib
 import time
+import ConfigParser
 	
+def getconfig():
+	Config=ConfigParser.ConfigParser()
+	try:
+		Config.read('smtpd-dual-delivery.cfg')
+
+		bindip=Config.get('daemon','bindip')
+		bindport=Config.get('daemon','bindport')
+		
+		destip_a=Config.get('destination_a','ip')
+		destport_a=Config.get('destination_a','port')
+
+		destip_b=Config.get('destination_b','ip')
+		destport_b=Config.get('destination_b','port')
+
+		return(bindip,bindport,destip_a,destport_a,destip_b,destport_b)
+	except:
+		exit('Configuration error')
+
 def logtime():
 	return time.strftime('%D %H:%M:%S')
 
-def proxymailto(ip,port,mailfrom,rcpttos,data):
+def deliverto(ip,port,mailfrom,rcpttos,data):
 
 	print '%s Relay mail to %s:%s' % (logtime(),ip, port)
 
@@ -50,16 +57,18 @@ class CustomSMTPServer(smtpd.SMTPServer):
 	print '%s Start Proxy action' %logtime()
 	print '%s Message from %s to %s size %s via %s' % (logtime(),mailfrom,rcpttos,len(data),peer)
 
-	state_a = proxymailto(proxytoip_a,proxytoport_a,mailfrom,rcpttos,data,peer)
+	state_a = deliverto(destip_a,destport_a,mailfrom,rcpttos,data)
 	print '%s %s' % (logtime(),state_a)
 
-	state_b = proxymailto(proxytoip_b,proxytoport_b,mailfrom,rcpttos,data,peer)
+	state_b = deliverto(destip_b,destport_b,mailfrom,rcpttos,data)
 	print '%s %s' % (logtime(),state_b)
 
 	print '%s End Proxy action' %logtime()
 
 	return
 
-server = CustomSMTPServer((bindip, bindport), None)
+(bindip,bindport,destip_a,destport_a,destip_b,destport_b)=getconfig()
+
+server = CustomSMTPServer((bindip, int(bindport)), None)
 
 asyncore.loop()
